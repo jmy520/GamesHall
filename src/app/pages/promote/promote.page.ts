a92550caaf7af0d4f6b167373d6b33df76c73ddb
Jason Jiang <71903875@qq.com>
2019-04-19 19:20:55 +0800

    新增推广tab页面

--- src/app/pages/promote/promote.page.ts
+++ src/app/pages/promote/promote.page.ts
@@ -7,10 +7,57 @@ import { Router } from '@angular/router';
   styleUrls: ['./promote.page.scss'],
 })
 export class PromotePage implements OnInit {
+  tabIndex: number = 0;
+
+  dataTotal: number = 0;
 
   constructor(public mRouter: Router) { }
 
   ngOnInit() {
   }
 
+  switchTab(tabIndex: number) {
+    this.tabIndex  = tabIndex;
+    switch(tabIndex) {
+      case 0:
+      this.doTab00Request();
+      break;
+
+      case 1:
+      this.doTab01Request();
+      break;
+
+      case 2:
+      this.doTab02Request();
+      break;
+
+      case 3:
+      this.doTab03Request();
+      break;
+
+      case 4:
+      this.doTab04Request();
+      break;
+    }
+  }
+
+  doTab00Request() {
+
+  }
+
+  doTab01Request() {
+
+  }
+
+  doTab02Request() {
+
+  }
+
+  doTab03Request() {
+
+  }
+
+  doTab04Request() {
+
+  }
 }
