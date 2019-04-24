16ef7b5b477790fd82d2525410fde07bdd18d680
Jason Jiang <71903875@qq.com>
2019-04-25 07:59:03 +0800

    新增个人中心页面

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,8 @@ const routes: Routes = [
   { path: 'refresh-chip', loadChildren: './pages/refresh-chip/refresh-chip.module#RefreshChipPageModule' },
   { path: 'safe-box', loadChildren: './pages/safe-box/safe-box.module#SafeBoxPageModule' },
   { path: 'withdrawal', loadChildren: './pages/withdrawal/withdrawal.module#WithdrawalPageModule' },
-  { path: 'game', loadChildren: './pages/game/game.module#GamePageModule' },
+  { path: 'game', loadChildren: './pages/game/game.module#GamePageModule' },  { path: 'personal-center', loadChildren: './pages/personal-center/personal-center.module#PersonalCenterPageModule' },
+
 
 ];
 
